54808a046564358404008efc94559c7e7f4cfddc
bpatrick24 <bpbrill98@gmail.com>
2023-02-01 17:00:12 -0500

    Add google map api and pinpoint on map

---
+++ main.js
@@ -0,0 +1,14 @@
+function initMap() {
+  const location = {lat: 39.93745077377082, lng: -75.02371407745103};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location
+  });
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
+
+
+
