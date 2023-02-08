061ca32d6db97ec1d0c8c9167b2f1e00b0821236
bpatrick24 <bpbrill98@gmail.com>
2023-02-08 14:13:13 -0500

    Use post method to send data from form to google sheet

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ function initMap() {
 }
 // End Map
 
+// Begin Form
+
+let form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  document.querySelector('#sub').value = "Submitting.";
+   let data = new FormData(form);
+   fetch('https://script.google.com/macros/s/AKfycbyDaQM-E376ckxcsTuisQSq2pitK3Ow1GQMHRT02HEhYt5VWHbs7U25H4PXzZf5nph_HQ/exec', {
+    method: "POST",
+    body: data
+   })
+   .then(res => res.text())
+   .then(data => {
+    document.querySelector('#msg').innerHTML = data;
+    document.querySelector('#sub').value = "Submit";
+   });
+})
+
+//End Form
